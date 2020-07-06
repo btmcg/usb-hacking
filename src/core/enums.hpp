2b71970a9da4312ad271510ea16b966a113abba8
brian <brian@eolas>
2020-07-06 19:25:09 -0400

    [core,usb-hw] emulate lsusb

---
+++ src/core/enums.hpp
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <fmt/format.h>
+#include <libusb.h>
+#include <cstdint>
+#include <string>
+
+
+// enum-to-string functions
+constexpr char const* to_str(libusb_class_code) noexcept;
+constexpr char const* to_str(libusb_iso_sync_type) noexcept;
+constexpr char const* to_str(libusb_iso_usage_type) noexcept;
+constexpr char const* to_str(libusb_transfer_type) noexcept;
+
+// endpoint_descriptor helpers
+constexpr char const* ep_addr_to_direction_str(std::uint8_t b_endpoint_address) noexcept;
+constexpr std::uint8_t ep_addr_to_ep_num(std::uint8_t b_endpoint_address) noexcept;
+constexpr libusb_transfer_type ep_attr_to_transfer_type(std::uint8_t bm_attributes) noexcept;
+constexpr libusb_iso_sync_type ep_attr_to_iso_sync_type(std::uint8_t bm_attributes) noexcept;
+constexpr libusb_iso_usage_type ep_attr_to_iso_usage_type(std::uint8_t bm_attributes) noexcept;
+
+// misc helpers
+inline std::string to_version(std::uint16_t v);
+
+
+/**********************************************************************/
+
+
+constexpr char const*
+to_str(libusb_class_code c) noexcept
+{
+    // clang-format off
+    switch (c) {
+        case LIBUSB_CLASS_PER_INTERFACE:        return "interface-specific";
+        case LIBUSB_CLASS_AUDIO:                return "audio";
+        case LIBUSB_CLASS_COMM:                 return "communications";
+        case LIBUSB_CLASS_HID:                  return "human interface device";
+        case LIBUSB_CLASS_PHYSICAL:             return "physical";
+        case LIBUSB_CLASS_PRINTER:              return "printer";
+        case LIBUSB_CLASS_PTP:                  return "image";
+        case LIBUSB_CLASS_MASS_STORAGE:         return "mass storage";
+        case LIBUSB_CLASS_HUB:                  return "hub";
+        case LIBUSB_CLASS_DATA:                 return "data";
+        case LIBUSB_CLASS_SMART_CARD:           return "smart card";
+        case LIBUSB_CLASS_CONTENT_SECURITY:     return "content security";
+        case LIBUSB_CLASS_VIDEO:                return "video";
+        case LIBUSB_CLASS_PERSONAL_HEALTHCARE:  return "personal healthcare";
+        case LIBUSB_CLASS_DIAGNOSTIC_DEVICE:    return "diagnostic device";
+        case LIBUSB_CLASS_WIRELESS:             return "wireless";
+        case LIBUSB_CLASS_APPLICATION:          return "application";
+        case LIBUSB_CLASS_VENDOR_SPEC:          return "vendor-specific";
+    }
+    // clang-format on
+
+    return "unknown";
+}
+
+constexpr char const*
+to_str(libusb_iso_sync_type t) noexcept
+{
+    // clang-format off
+    switch (t) {
+        case LIBUSB_ISO_SYNC_TYPE_NONE:     return "no synchronization";
+        case LIBUSB_ISO_SYNC_TYPE_ASYNC:    return "asynchronous";
+        case LIBUSB_ISO_SYNC_TYPE_ADAPTIVE: return "adaptive";
+        case LIBUSB_ISO_SYNC_TYPE_SYNC:     return "synchronous";
+    }
+    // clang-format on
+
+    return "unknown";
+}
+
+constexpr char const*
+to_str(libusb_iso_usage_type t) noexcept
+{
+    // clang-format off
+    switch (t) {
+        case LIBUSB_ISO_USAGE_TYPE_DATA:        return "data endpoint";
+        case LIBUSB_ISO_USAGE_TYPE_FEEDBACK:    return "feedback endpoint";
+        case LIBUSB_ISO_USAGE_TYPE_IMPLICIT:    return "implicit feedback endpoint";
+    }
+    // clang-format on
+    return "unknown";
+}
+
+constexpr char const*
+to_str(libusb_transfer_type t) noexcept
+{
+    // clang-format off
+    switch (t) {
+        case LIBUSB_TRANSFER_TYPE_CONTROL:      return "control";
+        case LIBUSB_TRANSFER_TYPE_ISOCHRONOUS:  return "isochronous";
+        case LIBUSB_TRANSFER_TYPE_BULK:         return "bulk";
+        case LIBUSB_TRANSFER_TYPE_INTERRUPT:    return "interrupt";
+        case LIBUSB_TRANSFER_TYPE_BULK_STREAM:  return "stream";
+    }
+    // clang-format on
+
+    return "unknown";
+}
+
+constexpr char const*
+ep_addr_to_direction_str(std::uint8_t addr) noexcept
+{
+    return ((addr >> 7) & 1u) ? "host-to-device" : "device-to-host";
+}
+
+constexpr std::uint8_t
+ep_addr_to_ep_num(std::uint8_t addr) noexcept
+{
+    return addr & 0b0000'0111;
+}
+
+constexpr libusb_transfer_type
+ep_attr_to_transfer_type(std::uint8_t t) noexcept
+{
+    return static_cast<libusb_transfer_type>(t & 0b0000'0011);
+}
+
+constexpr libusb_iso_sync_type
+ep_attr_to_iso_sync_type(std::uint8_t t) noexcept
+{
+    return static_cast<libusb_iso_sync_type>(t & 0b0000'1100);
+}
+
+constexpr libusb_iso_usage_type
+ep_attr_to_iso_usage_type(std::uint8_t t) noexcept
+{
+    return static_cast<libusb_iso_usage_type>(t & 0b0011'0000);
+}
+
+std::string
+to_version(std::uint16_t v)
+{
+    if (v == 0)
+        return "0";
+
+    std::string ver = fmt::format("{:x}", v);
+    return ver.insert(1, 1, '.');
+}
--- src/usb-hw/main.cpp
+++ src/usb-hw/main.cpp
@@ -1,3 +1,4 @@
+#include "core/enums.hpp"
 #include "util/assert.hpp"
 #include <fmt/format.h>
 #include <libusb.h>
@@ -6,111 +7,105 @@
 #include <cstdlib>
 
 
-constexpr char const*
-class_code_to_str(libusb_class_code c)
+bool
+print_endpoint_desc(libusb_endpoint_descriptor const* epd)
 {
-    switch (c) {
-        // clang-format off
-        case LIBUSB_CLASS_PER_INTERFACE:        return "interface-specific";
-        case LIBUSB_CLASS_AUDIO:                return "audio";
-        case LIBUSB_CLASS_COMM:                 return "communications";
-        case LIBUSB_CLASS_HID:                  return "human interface device";
-        case LIBUSB_CLASS_PHYSICAL:             return "physical";
-        case LIBUSB_CLASS_PRINTER:              return "printer";
-        case LIBUSB_CLASS_PTP:                  return "image";
-        case LIBUSB_CLASS_MASS_STORAGE:         return "mass storage";
-        case LIBUSB_CLASS_HUB:                  return "hub";
-        case LIBUSB_CLASS_DATA:                 return "data";
-        case LIBUSB_CLASS_SMART_CARD:           return "smart card";
-        case LIBUSB_CLASS_CONTENT_SECURITY:     return "content security";
-        case LIBUSB_CLASS_VIDEO:                return "video";
-        case LIBUSB_CLASS_PERSONAL_HEALTHCARE:  return "personal healthcare";
-        case LIBUSB_CLASS_DIAGNOSTIC_DEVICE:    return "diagnostic device";
-        case LIBUSB_CLASS_WIRELESS:             return "wireless";
-        case LIBUSB_CLASS_APPLICATION:          return "application";
-        case LIBUSB_CLASS_VENDOR_SPEC:          return "vendor-specific";
-            // clang-format on
-    }
-    return "unknown";
-}
+    DEBUG_ASSERT(epd != nullptr);
+    DEBUG_ASSERT(epd->bDescriptorType == LIBUSB_DT_ENDPOINT);
+    DEBUG_ASSERT(epd->bLength == 7);
 
-constexpr char const*
-direction_str(std::uint8_t addr)
-{
-    return ((addr >> 7) & 1u) ? "host-to-device" : "device-to-host";
-}
-
-constexpr std::uint8_t
-addr_num(std::uint8_t addr)
-{
-    return addr & 0b0000111;
-}
-
-constexpr libusb_transfer_type
-to_transfer_type(std::uint8_t t)
-{
-    return static_cast<libusb_transfer_type>(t & 0b0000'0011);
-}
-
-constexpr libusb_iso_sync_type
-to_iso_sync_type(std::uint8_t t)
-{
-    return static_cast<libusb_iso_sync_type>(t & 0b0000'1100);
-}
-
-constexpr char const*
-iso_sync_type_to_str(libusb_iso_sync_type t) noexcept
-{
-    switch (t) {
-        // clang-format off
-        case LIBUSB_ISO_SYNC_TYPE_NONE:     return "no synchronization";
-        case LIBUSB_ISO_SYNC_TYPE_ASYNC:    return "asynchronous";
-        case LIBUSB_ISO_SYNC_TYPE_ADAPTIVE: return "adaptive";
-        case LIBUSB_ISO_SYNC_TYPE_SYNC:     return "synchronous";
-            // clang-format on
+    std::string attrs(to_str(ep_attr_to_transfer_type(epd->bmAttributes)));
+    if (ep_attr_to_transfer_type(epd->bmAttributes) == LIBUSB_TRANSFER_TYPE_ISOCHRONOUS) {
+        attrs.append(",");
+        attrs.append(to_str(ep_attr_to_iso_sync_type(epd->bmAttributes)));
+        attrs.append(",");
+        attrs.append(to_str(ep_attr_to_iso_usage_type(epd->bmAttributes)));
     }
 
-    return "unknown";
-}
+    fmt::print("              number:            {} ({})\n"
+               "              attrs:             {}\n"
+               "              max packet size:   {}\n"
+               "              interval:          {}\n"
+               "              refresh:           {}\n"
+               "              sync address:      {}\n"
+               "              unknown endpoints: {}\n",
+            ep_addr_to_ep_num(epd->bEndpointAddress),
+            ep_addr_to_direction_str(epd->bEndpointAddress),
+            to_str(ep_attr_to_transfer_type(epd->bmAttributes)), epd->wMaxPacketSize,
+            epd->bInterval, epd->bRefresh, epd->bSynchAddress, epd->extra_length);
 
-constexpr libusb_iso_usage_type
-to_iso_usage_type(std::uint8_t t)
-{
-    return static_cast<libusb_iso_usage_type>(t & 0b0011'0000);
-}
-
-constexpr char const*
-iso_usage_type_to_str(libusb_iso_usage_type t) noexcept
-{
-    switch (t) {
-        // clang-format off
-        case LIBUSB_ISO_USAGE_TYPE_DATA:        return "data endpoint";
-        case LIBUSB_ISO_USAGE_TYPE_FEEDBACK:    return "feedback endpoint";
-        case LIBUSB_ISO_USAGE_TYPE_IMPLICIT:    return "implicit feedback endpoint";
-            // clang-format on
-    }
-
-    return "unknown";
-}
-
-constexpr char const*
-transfer_type_to_str(libusb_transfer_type t)
-{
-    switch (t) {
-        // clang-format off
-        case LIBUSB_TRANSFER_TYPE_CONTROL:      return "control";
-        case LIBUSB_TRANSFER_TYPE_ISOCHRONOUS:  return "isochronous";
-        case LIBUSB_TRANSFER_TYPE_BULK:         return "bulk";
-        case LIBUSB_TRANSFER_TYPE_INTERRUPT:    return "interrupt";
-        case LIBUSB_TRANSFER_TYPE_BULK_STREAM:  return "stream";
-            // clang-format on
-    }
-    return "unknown";
+    return true;
 }
 
 bool
-print_desc(libusb_device* dev)
+print_interface_desc(libusb_interface_descriptor const* ifd)
 {
+    DEBUG_ASSERT(ifd != nullptr);
+    DEBUG_ASSERT(ifd->bDescriptorType == LIBUSB_DT_INTERFACE);
+    DEBUG_ASSERT(ifd->bLength == 9);
+
+    fmt::print("          number:             {}\n"
+               "          alternate setting:  {}\n"
+               "          class:              {}\n"
+               "          sub-class:          {}\n"
+               "          protocol:           {}\n"
+               "          interface index:    {}\n"
+               "          unknown interfaces: {}\n"
+               "          num endpoints:      {}\n",
+            ifd->bInterfaceNumber, ifd->bAlternateSetting,
+            to_str(static_cast<libusb_class_code>(ifd->bInterfaceClass)),
+            to_str(static_cast<libusb_class_code>(ifd->bInterfaceSubClass)),
+            ifd->bInterfaceProtocol, ifd->iInterface, ifd->extra_length, ifd->bNumEndpoints);
+
+    for (int ep_num = 0; ep_num < ifd->bNumEndpoints; ++ep_num) {
+        libusb_endpoint_descriptor const epd = ifd->endpoint[ep_num];
+
+        fmt::print("            endpoint {}:\n", ep_num);
+        if (!print_endpoint_desc(&epd))
+            return false;
+    }
+
+    return true;
+}
+
+bool
+print_config_desc(libusb_config_descriptor const* cd)
+{
+    DEBUG_ASSERT(cd != nullptr);
+    DEBUG_ASSERT(cd->bDescriptorType == LIBUSB_DT_CONFIG);
+    DEBUG_ASSERT(cd->bLength == 9);
+
+    fmt::print("      total length:    {}\n"
+               "      config value:    {}\n"
+               "      config:          {}\n"
+               "      attributes:      {}\n"
+               "      max power:       {}\n"
+               "      unknown configs: {}\n"
+               "      num interfaces:  {}\n",
+            cd->wTotalLength, cd->bConfigurationValue, cd->iConfiguration, cd->bmAttributes,
+            cd->MaxPower, cd->extra_length, cd->bNumInterfaces);
+
+    if (cd->bNumInterfaces > 0) {
+        libusb_interface const* iface = cd->interface;
+
+        for (int iface_num = 0; iface_num < iface->num_altsetting; ++iface_num) {
+            libusb_interface_descriptor const ifd = iface->altsetting[iface_num];
+            fmt::print("        interface {}:\n", iface_num);
+            if (!print_interface_desc(&ifd))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool
+print_device_desc(libusb_device* dev)
+{
+    DEBUG_ASSERT(dev != nullptr);
+
+    bool success = true;
+
     libusb_device_descriptor dd;
     int rv = ::libusb_get_device_descriptor(dev, &dd);
     if (rv != 0) {
@@ -121,21 +116,21 @@ print_desc(libusb_device* dev)
     DEBUG_ASSERT(dd.bLength == 18);
     DEBUG_ASSERT(dd.bDescriptorType == LIBUSB_DT_DEVICE);
 
-    fmt::print("    usb spec release:   {:#x}\n"
-               "    class:              {}\n"
-               "    subclass:           {}\n"
-               "    protocol:           {}\n"
-               "    max packet size:    {}\n"
-               "    vendor id:          {}\n"
-               "    product id:         {}\n"
-               "    device release:     {:#x}\n"
-               "    manufacturer:       {}\n"
-               "    product:            {}\n"
-               "    serial number:      {}\n"
-               "    num configurations: {}\n",
-            dd.bcdUSB, class_code_to_str(static_cast<libusb_class_code>(dd.bDeviceClass)),
-            class_code_to_str(static_cast<libusb_class_code>(dd.bDeviceSubClass)),
-            dd.bDeviceProtocol, dd.bMaxPacketSize0, dd.idVendor, dd.idProduct, dd.bcdDevice,
+    fmt::print("  usb spec release:   {}\n"
+               "  class:              {}\n"
+               "  subclass:           {}\n"
+               "  protocol:           {}\n"
+               "  max packet size:    {}\n"
+               "  vendor id:          {:#06x}\n"
+               "  product id:         {:#06x}\n"
+               "  device release:     {}\n"
+               "  manufacturer:       {}\n"
+               "  product:            {}\n"
+               "  serial number:      {}\n"
+               "  num configurations: {}\n",
+            to_version(dd.bcdUSB), to_str(static_cast<libusb_class_code>(dd.bDeviceClass)),
+            to_str(static_cast<libusb_class_code>(dd.bDeviceSubClass)), dd.bDeviceProtocol,
+            dd.bMaxPacketSize0, dd.idVendor, dd.idProduct, to_version(dd.bcdDevice),
             dd.iManufacturer, dd.iProduct, dd.iSerialNumber, dd.bNumConfigurations);
 
     for (int config_num = 0; config_num < dd.bNumConfigurations; ++config_num) {
@@ -143,81 +138,22 @@ print_desc(libusb_device* dev)
         rv = ::libusb_get_config_descriptor(dev, config_num, &cd);
         if (rv != 0) {
             fmt::print(stderr, "libusb_get_config_descriptor failure ({})\n", rv);
-            return false;
+            success = false;
+            break;
         }
 
-        DEBUG_ASSERT(cd->bDescriptorType == LIBUSB_DT_CONFIG);
-        DEBUG_ASSERT(cd->bLength == 9);
-        fmt::print("       configuration {}:\n"
-                   "           total length:    {}\n"
-                   "           config value:    {}\n"
-                   "           config:          {}\n"
-                   "           attributes:      {}\n"
-                   "           max power:       {}\n"
-                   "           unknown configs: {}\n"
-                   "           num interfaces:  {}\n",
-                config_num, cd->wTotalLength, cd->bConfigurationValue, cd->iConfiguration,
-                cd->bmAttributes, cd->MaxPower, cd->extra_length, cd->bNumInterfaces);
+        fmt::print("    configuration {}:\n", config_num);
 
-        if (cd->bNumInterfaces > 0) {
-            libusb_interface const* iface = cd->interface;
-            for (int iface_num = 0; iface_num < iface->num_altsetting; ++iface_num) {
-                libusb_interface_descriptor const ifd = iface->altsetting[iface_num];
-
-                DEBUG_ASSERT(ifd.bDescriptorType == LIBUSB_DT_INTERFACE);
-                DEBUG_ASSERT(ifd.bLength == 9);
-                fmt::print("               interface {}:\n"
-                           "                   number:             {}\n"
-                           "                   alternate setting:  {}\n"
-                           "                   class:              {}\n"
-                           "                   sub-class:          {}\n"
-                           "                   protocol:           {}\n"
-                           "                   interface index:    {}\n"
-                           "                   unknown interfaces: {}\n"
-                           "                   num endpoints:      {}\n",
-                        iface_num, ifd.bInterfaceNumber, ifd.bAlternateSetting,
-                        class_code_to_str(static_cast<libusb_class_code>(ifd.bInterfaceClass)),
-                        class_code_to_str(static_cast<libusb_class_code>(ifd.bInterfaceSubClass)),
-                        ifd.bInterfaceProtocol, ifd.iInterface, ifd.extra_length,
-                        ifd.bNumEndpoints);
-
-                if (ifd.bNumEndpoints > 0) {
-                    for (int ep_num = 0; ep_num < ifd.bNumEndpoints; ++ep_num) {
-                        libusb_endpoint_descriptor const epd = ifd.endpoint[ep_num];
-
-                        DEBUG_ASSERT(epd.bDescriptorType == LIBUSB_DT_ENDPOINT);
-                        DEBUG_ASSERT(epd.bLength == 7);
-                        std::string attrs(transfer_type_to_str(to_transfer_type(epd.bmAttributes)));
-                        if (to_transfer_type(epd.bmAttributes)
-                                == LIBUSB_TRANSFER_TYPE_ISOCHRONOUS) {
-                            attrs.append(",");
-                            attrs.append(iso_sync_type_to_str(to_iso_sync_type(epd.bmAttributes)));
-                            attrs.append(",");
-                            attrs.append(
-                                    iso_usage_type_to_str(to_iso_usage_type(epd.bmAttributes)));
-                        }
-                        fmt::print("                       endpoint {}:\n"
-                                   "                           address:           {} ({})\n"
-                                   "                           attrs:             {}\n"
-                                   "                           max packet size:   {}\n"
-                                   "                           interval:          {}\n"
-                                   "                           refresh:           {}\n"
-                                   "                           sync address:      {}\n"
-                                   "                           unknown endpoints: {}\n",
-                                ep_num, addr_num(epd.bEndpointAddress),
-                                direction_str(epd.bEndpointAddress),
-                                transfer_type_to_str(to_transfer_type(epd.bmAttributes)),
-                                epd.wMaxPacketSize, epd.bInterval, epd.bRefresh, epd.bSynchAddress,
-                                epd.extra_length);
-                    }
-                }
-            }
+        if (!print_config_desc(cd)) {
+            success = false;
+            ::libusb_free_config_descriptor(cd);
+            break;
         }
 
         ::libusb_free_config_descriptor(cd);
     }
 
-    return true;
+    return success;
 }
 
 int
@@ -250,7 +186,7 @@ main()
     fmt::print("found {} devices:\n", num_devs);
     for (ssize_t i = 0; i < num_devs; ++i) {
         fmt::print("device {}:\n", i);
-        if (!print_desc(devices[i])) {
+        if (!print_device_desc(devices[i])) {
             ::libusb_exit(ctx);
             ::libusb_free_device_list(devices, 1);
             std::exit(EXIT_FAILURE);
