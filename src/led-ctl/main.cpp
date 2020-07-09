ef840f202d1ba77fa593195f33b6f7a825c809da
brian <brian@eolas>
2020-07-09 14:39:34 -0400

    [led-ctl] initial working prototype

--- src/led-ctl/main.cpp
+++ src/led-ctl/main.cpp
@@ -45,11 +45,15 @@ main(int argc, char** argv)
         std::exit(EXIT_FAILURE);
     }
 
-    int const kernel_attached = ::libusb_kernel_driver_active(dev_handle, 0);
-    if (kernel_attached == 0) {
+    if (int kernel_attached = ::libusb_kernel_driver_active(dev_handle, 0); kernel_attached == 0) {
         fmt::print("no kernel driver attached\n");
     } else if (kernel_attached == 1) {
-        fmt::print("kernel is attached\n");
+        if (int rv = ::libusb_detach_kernel_driver(dev_handle, 0); rv != 0) {
+            fmt::print(stderr, "libusb: device could not be found or permission denied\n");
+            ::libusb_close(dev_handle);
+            ::libusb_exit(ctx);
+            std::exit(EXIT_FAILURE);
+        }
     } else {
         fmt::print(stderr, "libusb_kernel_driver_active: failure ({})\n",
                 ::libusb_strerror(static_cast<libusb_error>(kernel_attached)));
@@ -58,7 +62,55 @@ main(int argc, char** argv)
         std::exit(EXIT_FAILURE);
     }
 
-    // ::libusb_detach_kernel_driver(dev_handle, 0)
+    if (int rv = ::libusb_claim_interface(dev_handle, /*interface=*/0); rv != 0) {
+        fmt::print(stderr, "libusb_claim_interface: failure ({})\n",
+                ::libusb_strerror(static_cast<libusb_error>(rv)));
+        ::libusb_close(dev_handle);
+        ::libusb_exit(ctx);
+        std::exit(EXIT_FAILURE);
+    }
+
+
+    std::uint8_t buf[8] = {};
+    buf[0] = 101; // major cmd (8-byte write command)
+    buf[1] = 2; // minor cmd (write port 1)
+    buf[2] = 0x03; // lsb (port 1 value of 0x03)
+
+    fmt::print("send: [");
+    for (int i = 0; i < 8; ++i) {
+        fmt::print("{:#04x} ", buf[i]);
+    }
+    fmt::print("]\n");
+
+    for (int i = 0; i < 1000; ++i) {
+        if (int rv = ::libusb_control_transfer(dev_handle,
+                    LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE | LIBUSB_ENDPOINT_OUT,
+                    0x09 /*HID Set_Report*/, (3 /*HID feature*/ << 8) | buf[0],
+                    /*interface=*/0, buf, sizeof(buf), 1000 /*timeout millis*/);
+                rv <= 0) {
+            fmt::print(stderr, "libusb_control_transfer: failure ({})\n",
+                    ::libusb_strerror(static_cast<libusb_error>(rv)));
+        }
+    }
+
+    fmt::print("recv: [");
+    for (int i = 0; i < 8; ++i) {
+        fmt::print("{:#04x} ", buf[i]);
+    }
+    fmt::print("]\n");
+
+
+    //
+    // shutdown/disconnect sequence
+    //
+
+    if (int rv = ::libusb_release_interface(dev_handle, /*interface=*/0); rv != 0) {
+        fmt::print(stderr, "libusb_release_interface: failure ({})\n",
+                ::libusb_strerror(static_cast<libusb_error>(rv)));
+        ::libusb_close(dev_handle);
+        ::libusb_exit(ctx);
+        std::exit(EXIT_FAILURE);
+    }
 
     ::libusb_close(dev_handle);
     ::libusb_exit(ctx);
