28a020d1a5f7f6509a3695dc11f2511b414975b0
brian <brian@eolas>
2020-07-13 12:18:42 -0400

    [led-ctl] fix documentation

--- src/led-ctl/delcom_protocol.hpp
+++ src/led-ctl/delcom_protocol.hpp
@@ -72,82 +72,88 @@ namespace delcom {
             /// port pin(s) takes precendence over setting.
             SetOrResetPort0 = 11,
 
-            /// Sets or resets the port 1 pins individually. The LSB resets
-            /// the corresponding port pin(s) and the MSB sets the
-            /// corresponding port pin(s) on port 1. Resetting the port pin(s)
-            /// takes precendence over setting.
+            /// Sets or resets the port 1 pins individually. The LSB
+            /// resets the corresponding port pin(s) and the MSB sets
+            /// the corresponding port pin(s) on port 1. Resetting the
+            /// port pin(s) takes precendence over setting.
             SetOrResetPort1 = 12,
 
-            /// Loads the Clock Generator Global Prescalar value. This value
-            /// is passed in the LSB register. Increasing this number
-            /// decreases all the clock function frequencies. Prescalar range
-            /// is 1 to 255 and the boot up default value is 10. This is the
-            /// master prescalar for all the LED pins. Increasing this
-            /// number will slow down the flash rate.
+            /// Loads the Clock Generator Global Prescalar value. This
+            /// value is passed in the LSB register. Increasing this
+            /// number decreases all the clock function frequencies.
+            /// Prescalar range is 1 to 255 and the boot up default
+            /// value is 10. This is the master prescalar for all the
+            /// LED pins. Increasing this number will slow down the
+            /// flash rate.
             SetClockGen = 19,
 
-            /// Enables or disables the clock generator on port 1. The lower
-            /// nibble of the LSB disables the corresponding port pin(s) and
-            /// the lower nibble of the MSB enables the corresponding port
-            /// pin(s). Disabling the port pin(s) takes precedence over
-            /// enabling. This command enables the flash mode on the
-            /// appropriate LED pin. Note you must also turn the LED pin on
-            /// with one of the port control commands (e.g. 101-2 or
-            /// 101-12).
+            /// Enables or disables the clock generator on port 1. The
+            /// lower nibble of the LSB disables the corresponding port
+            /// pin(s) and the lower nibble of the MSB enables the
+            /// corresponding port pin(s). Disabling the port pin(s)
+            /// takes precedence over enabling. This command enables the
+            /// flash mode on the appropriate LED pin. Note you must
+            /// also turn the LED pin on with one of the port control
+            /// commands (e.g. 101-2 or 101-12).
             ToggleClockGenPort1 = 20,
 
-            /// Loads the frequency and duty cycle for port 1 pin 0. The LSB
-            /// data sets the high duty cycle and the MSB data sets the low
-            /// duty cycle on port 1 pin 0. This command will set the flash
-            /// rate and duty cycle on the LED pin.
+            /// Loads the frequency and duty cycle for port 1 pin 0. The
+            /// LSB data sets the high duty cycle and the MSB data sets
+            /// the low duty cycle on port 1 pin 0. This command will
+            /// set the flash rate and duty cycle on the LED pin.
             SetDutyCyclePort1Pin0 = 21,
 
-            /// Loads the frequency and duty cycle for port 1 pin 1. The LSB
-            /// data sets the high duty cycle and the MSB data sets the low
-            /// duty cycle on port 1 pin 1. This command will set the flash
-            /// rate and duty cycle on the LED pin.
+            /// Loads the frequency and duty cycle for port 1 pin 1. The
+            /// LSB data sets the high duty cycle and the MSB data sets
+            /// the low duty cycle on port 1 pin 1. This command will
+            /// set the flash rate and duty cycle on the LED pin.
             SetDutyCyclePort1Pin1 = 22,
 
-            /// Loads the frequency and duty cycle for port 1 pin 2. The LSB
-            /// data sets the high duty cycle and the MSB data sets the low
-            /// duty cycle on port 1 pin 2. This command will set the flash
-            /// rate and duty cycle on the LED pin.
+            /// Loads the frequency and duty cycle for port 1 pin 2. The
+            /// LSB data sets the high duty cycle and the MSB data sets
+            /// the low duty cycle on port 1 pin 2. This command will
+            /// set the flash rate and duty cycle on the LED pin.
             SetDutyCyclePort1Pin2 = 23,
 
-            /// Synchronizes the clock generation. This command synchronizes
-            /// all the clock generators to start now plus an initial phase
-            /// delay, see below. The lower nibble of the LSB enables this
-            /// function on the corresponding pins P1.0 to P1.3. The lower
-            /// nibble of the MSB presets the initial value on the
-            /// corresponding pins P1.0 to P1.3. Initial phase delay
-            /// resolution is in 10ms and is passed in the LSB register.
-            /// Initial phase delay registers are cleared after this command
-            /// is sent. Therefore the initial phase delay registers must be
-            /// set each time this command is called. Use this command to
-            /// sync a LED flash pattern.
+            /// Synchronizes the clock generation. This command
+            /// synchronizes all the clock generators to start now plus
+            /// an initial phase delay, see below. The lower nibble of
+            /// the LSB enables this function on the corresponding pins
+            /// P1.0 to P1.3. The lower nibble of the MSB presets the
+            /// initial value on the corresponding pins P1.0 to P1.3.
+            /// Initial phase delay resolution is in 10ms and is passed
+            /// in the LSB register. Initial phase delay registers are
+            /// cleared after this command is sent. Therefore the
+            /// initial phase delay registers must be set each time this
+            /// command is called. Use this command to sync a LED flash
+            /// pattern.
             SyncClockGen = 25,
 
-            /// Load initial phase delay on port 1 pin 0. Sets the LSB data
-            /// as the port1 pin0 initial delay value. To be used with command 25.
+            /// Load initial phase delay on port 1 pin 0. Sets the LSB
+            /// data as the port1 pin0 initial delay value. To be used
+            /// with command 25.
             SetInitialPhaseDelayPort1Pin0 = 26,
 
-            /// Load initial phase delay on port 1 pin 1. Sets the LSB data
-            /// as the port1 pin0 initial delay value. To be used with command 25.
+            /// Load initial phase delay on port 1 pin 1. Sets the LSB
+            /// data as the port1 pin0 initial delay value. To be used
+            /// with command 25.
             SetInitialPhaseDelayPort1Pin1 = 27,
 
-            /// Load initial phase delay on port 1 pin 2. Sets the LSB data
-            /// as the port1 pin0 initial delay value. To be used with command 25.
+            /// Load initial phase delay on port 1 pin 2. Sets the LSB
+            /// data as the port1 pin0 initial delay value. To be used
+            /// with command 25.
             SetInitialPhaseDelayPort1Pin2 = 28,
 
-            /// Load initial phase delay on port 1 pin 3. Sets the LSB data
-            /// as the port1 pin0 initial delay value. To be used with command 25.
+            /// Load initial phase delay on port 1 pin 3. Sets the LSB
+            /// data as the port1 pin0 initial delay value. To be used
+            /// with command 25.
             SetInitialPhaseDelayPort1Pin3 = 29,
 
             /// Load the PWM values. Port pins P1.0 through P1.3 can be
             /// placed in PWM mode by writing the PWM value with this
             /// command. The LSB Data parameter is the port pin number,
-            /// range is 0-3. The MSB Data parameter is the PWM value, range
-            /// is 0-100. This command is used to set the power or
+            /// range is 0-3. The MSB Data parameter is the PWM value,
+            /// range is 0-100. This command is used to set the power or
             /// brightness for the LED pins. Default value is 80 (80%).
             SetPWM = 34,
 
@@ -160,20 +166,20 @@ namespace delcom {
             /// 0 pin 0.
             ToggleEventCounter = 38,
 
-            /// Buzzer Control. This command is used to drive a buzzer on
-            /// port 0 pin3. See the buzzer control description in the
-            /// USBIOHID datasheet for more information.
+            /// Buzzer Control. This command is used to drive a buzzer
+            /// on port 0 pin3. See the buzzer control description in
+            /// the USBIOHID datasheet for more information.
             BuzzerCtrl = 70,
 
             /// Auto Clear & Auto Confirm Control. This command
-            /// enables/disables the Auto Clear and Auto Confirm feature.
-            /// Bit 6 of the DataLSB controls the Auto Clear feature and bit
-            /// 7 controls the Auto Confirm feature. A high value enables
-            /// the feature and low disables it. When enabled, the auto
-            /// clear control will turn off all the LED pins when the
-            /// internal switch (P0.0) is pressed. When enabled, auto
-            /// confirm control will sound two quick buzzer tones to
-            /// indicate that the switch was pressed.
+            /// enables/disables the Auto Clear and Auto Confirm
+            /// feature. Bit 6 of the DataLSB controls the Auto Clear
+            /// feature and bit 7 controls the Auto Confirm feature. A
+            /// high value enables the feature and low disables it. When
+            /// enabled, the auto clear control will turn off all the
+            /// LED pins when the internal switch (P0.0) is pressed.
+            /// When enabled, auto confirm control will sound two quick
+            /// buzzer tones to indicate that the switch was pressed.
             AutoClearAutoConfirmCtrl = 72,
         };
 
@@ -195,9 +201,10 @@ namespace delcom {
             Command cmd;
         } PACKED;
 
-        /// Packet containing all commands to be sent to the VI HID. Both
-        /// Read and Write commands are written via recv_cmd and send_cmd
-        /// respectively. Underlying data accessible via data member.
+        /// Packet containing all commands to be sent to the VI HID.
+        /// Both Read and Write commands are written via recv_cmd and
+        /// send_cmd respectively. Underlying data accessible via data
+        /// member.
         union packet
         {
             std::uint8_t data[8] = {0};
