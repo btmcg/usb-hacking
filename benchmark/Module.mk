06952810f11c6ad3e74fbf8529ba13c6604950eb
brian <brian@eolas>
2020-07-20 09:35:00 -0400

    [benchmark] remove as its not needed

--- benchmark/Module.mk
+++
@@ -1,8 +0,0 @@
-# MODULE_NAME := benchmark-runner
-# MODULE_CPPFLAGS := -I.
-# MODULE_LIBRARIES :=
-
-# $(call use-fmt)
-# $(call use-google-benchmark)
-
-# $(call add-executable-module,$(call get-path))
